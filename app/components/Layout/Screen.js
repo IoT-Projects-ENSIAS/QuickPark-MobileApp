992c187a044cdcc68d9aa1c52541073fc035a309
D3vss <mohamed_oudoun@um5.ac.ma>
2022-04-19 01:16:35 +0000

    All set/ Work Started

---
+++ app/components/Layout/Screen.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { SafeAreaView, StyleSheet, StatusBar, Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
+
+function Screen({ children }, ...otherProps) {
+  return (
+    <SafeAreaView style={styles.screen} {...otherProps}>
+      <StatusBar backgroundColor="#002133" barStyle="light-content" />
+
+      {children}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    height: height,
+    width: width,
+  },
+});
+
+export default Screen;
