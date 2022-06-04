f92e52a33da2e4b7325fb928cdaeb7f7a18fbf0b
D3vss <mohamed_oudoun@um5.ac.ma>
2022-06-04 18:16:16 +0100

    Fake data

---
+++ app/Screens/Home/Data.Home.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    id: 1,
+    name: "Parking 1",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin, erat ut pretium posuere, ipsum lacus ornare nulla, ut consectetur nunc enim et arcu. Maecenas ultrices nulla sed leo eleifend, vitae volutpat neque sagittis.",
+    rating: "Good",
+    location: "101, Avenue Essalam (c.y.m.), bloc Tsf",
+    capacity: "90",
+    available: "20",
+    reviews: [
+      {
+        stars: 4,
+        opinion:
+          "Morbi augue nisi, euismod quis vulputate viverra, aliquet quis lorem. ",
+      },
+      {
+        stars: 3,
+        opinion:
+          "Morbi augue nisi, euismod quis vulputate viverra, aliquet quis lorem. ",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Parking 2",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin, erat ut pretium posuere, ipsum lacus ornare nulla, ut consectetur nunc enim et arcu. Maecenas ultrices nulla sed leo eleifend, vitae volutpat neque sagittis.",
+    rating: "Good",
+    location: "101, Avenue Essalam (c.y.m.), bloc Tsf",
+    capacity: "90",
+    available: "20",
+    reviews: [
+      {
+        stars: 4,
+        opinion:
+          "Morbi augue nisi, euismod quis vulputate viverra, aliquet quis lorem. ",
+      },
+      {
+        stars: 3,
+        opinion:
+          "Morbi augue nisi, euismod quis vulputate viverra, aliquet quis lorem. ",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Parking 3",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin, erat ut pretium posuere, ipsum lacus ornare nulla, ut consectetur nunc enim et arcu. Maecenas ultrices nulla sed leo eleifend, vitae volutpat neque sagittis.",
+    rating: "Good",
+    location: "101, Avenue Essalam (c.y.m.), bloc Tsf",
+    capacity: "90",
+    available: "20",
+    reviews: [
+      {
+        stars: 4,
+        opinion:
+          "Morbi augue nisi, euismod quis vulputate viverra, aliquet quis lorem. ",
+      },
+      {
+        stars: 3,
+        opinion:
+          "Morbi augue nisi, euismod quis vulputate viverra, aliquet quis lorem. ",
+      },
+    ],
+  },
+];
---
+++ app/Screens/Home/Data.User.js
@@ -0,0 +1,21 @@
+export default {
+  firstName: "John",
+  lastName: "Doe",
+  email: "email@email.com",
+  cars: [
+    {
+      id: 1,
+      model: "Honda Civic",
+    },
+    {
+      id: 2,
+      model: "Toyota Camry",
+    },
+  ],
+  favoriteParkings: [
+    //from database
+  ],
+  recentlyVisited: [
+    //from database
+  ],
+};
