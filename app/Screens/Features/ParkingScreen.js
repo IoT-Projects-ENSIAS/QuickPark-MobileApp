d700a3f1e025f4542fa9689315c3dd108247a439
D3vss <mohamed_oudoun@um5.ac.ma>
2022-06-04 19:10:08 +0100

    Parking screen done

--- app/Screens/Features/ParkingScreen.js
+++ app/Screens/Features/ParkingScreen.js
@@ -1,16 +1,42 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import { Card } from "@rneui/base";
 import Screen from "../../components/Layout/Screen";
+
+const { width, height } = Dimensions.get("window");
 
 function ParkingScreen({ navigation, route }) {
   const { parkingDetails } = route.params;
   return (
     <Screen route={route.name} screenName={"Parking 1"}>
       <View>
-        <Text>{parkingDetails.id}</Text>
+        <Card>
+          <Card.Title>{parkingDetails.name}</Card.Title>
+          <Card.Divider />
+          <Image
+            style={styles.image}
+            source={{
+              uri: parkingDetails.img,
+            }}
+          />
+          <Text>Description: {parkingDetails.desc}</Text>
+          <Text>Location: {parkingDetails.location}</Text>
+          <Text>Rating: {parkingDetails.rating}</Text>
+          <Text>Capacity: {parkingDetails.capacity}</Text>
+          <Text>Available spots: {parkingDetails.available}</Text>
+        </Card>
       </View>
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    alignSelf: "center",
+    resizeMode: "contain",
+    width: "100%",
+    height: height / 3,
+  },
+});
 
 export default ParkingScreen;
--- app/Screens/Home/Data.Home.js
+++ app/Screens/Home/Data.Home.js
@@ -2,6 +2,7 @@ export default [
   {
     id: 1,
     name: "Parking 1",
+    img: "https://www.ledgerinsights.com/wp-content/uploads/2020/05/parking-lot-cars.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin, erat ut pretium posuere, ipsum lacus ornare nulla, ut consectetur nunc enim et arcu. Maecenas ultrices nulla sed leo eleifend, vitae volutpat neque sagittis.",
     rating: "Good",
     location: "101, Avenue Essalam (c.y.m.), bloc Tsf",
@@ -23,9 +24,10 @@ export default [
   {
     id: 2,
     name: "Parking 2",
+    img: "https://circontrol.com/wp-content/uploads/2019/02/180125-Circontrol-BAIXA-80-1080x675.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin, erat ut pretium posuere, ipsum lacus ornare nulla, ut consectetur nunc enim et arcu. Maecenas ultrices nulla sed leo eleifend, vitae volutpat neque sagittis.",
     rating: "Good",
-    location: "101, Avenue Essalam (c.y.m.), bloc Tsf",
+    location: "14, avenue Al Oumam AlMouttahida -ex Nations Unies, Agdal",
     capacity: "90",
     available: "20",
     reviews: [
@@ -44,9 +46,10 @@ export default [
   {
     id: 3,
     name: "Parking 3",
+    img: "https://images.unsplash.com/photo-1506521781263-d8422e82f27a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGFya2luZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin, erat ut pretium posuere, ipsum lacus ornare nulla, ut consectetur nunc enim et arcu. Maecenas ultrices nulla sed leo eleifend, vitae volutpat neque sagittis.",
     rating: "Good",
-    location: "101, Avenue Essalam (c.y.m.), bloc Tsf",
+    location: "24, rue d' Essaouira",
     capacity: "90",
     available: "20",
     reviews: [
